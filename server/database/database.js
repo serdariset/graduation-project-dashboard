b168d45a94260e96eb2f587709202795360a5d89
serdar iset <mahmutserdarist@gmail.com>
2021-12-15 20:07:45 +0300

    test

--- server/database/database.js
+++ server/database/database.js
@@ -1,13 +1,19 @@
 const {Client} = require('pg')
 
 const client = new Client({
+    host:"tai.db.elephantsql.com",
+    user:"zawnrnno",
+    password:"1ejpeRVHmyzfIiAWyY6m29NddV_4EzOT",
+    database:"zawnrnno"
+})
+/* const client = new Client({
     host:"localhost",
     user:"postgres",
     port:"5432",
     password:"dashboard",
     database:"factory"
 })
-
+ */
 client.connect(()=>{
     console.log('PostgreSQL connected successfully')
 })
